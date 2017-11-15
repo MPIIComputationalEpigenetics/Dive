5c4e6c3340c8769ac31a4fc5c45df57741f9cdb2
Felipe Albrecht <felipe.albrecht@gmail.com>
2017-11-15 19:37:52 +0100

    include statistics processing in the frontend

--- src/app/view/screen/data-selection.ts
+++ src/app/view/screen/data-selection.ts
@@ -5,7 +5,9 @@ import { DeepBlueService } from "app/service/deepblue";
 import { SelectedData } from "app/service/selecteddata";
 import { IOperation } from 'app/domain/interfaces';
 import { ProgressElement } from 'app/service/progresselement';
-import { OverlapsBarChartComponent } from 'app/view/component/charts/overlappingbar';
+import { SimilarityBarChartComponent } from 'app/view/component/charts/similarity';
+import { DeepBlueMiddlewareOverlapEnrichtmentResultItem, DeepBlueMiddlewareOverlapResult } from 'app/domain/operations';
+import { Statistics, IStatsResult } from 'app/service/statistics';
 
 @Component({
     templateUrl: './data-selection.html'
@@ -14,36 +16,132 @@ export class DataSelectionScreen {
 
     selected_data: IOperation;
 
-    @ViewChild('biosourcessimilaritybarchart') biosourcessimilaritybarchart: OverlapsBarChartComponent;
-    @ViewChild('emssimilaritybarchart') emssimilaritybarchart: OverlapsBarChartComponent;
+    @ViewChild('biosourcessimilaritybarchart') biosourcessimilaritybarchart: SimilarityBarChartComponent;
+    @ViewChild('emssimilaritybarchart') emssimilaritybarchart: SimilarityBarChartComponent;
 
     constructor(private deepBlueService: DeepBlueService, public progress_element: ProgressElement,
         private selectedData: SelectedData) {
     }
+
+    visibleSidebar2 = false;
 
     selectQuery(event: IOperation) {
         this.selected_data = event;
         this.deepBlueService.setDataToDive(this.selected_data);
         this.deepBlueService.composedCalculateFastsEnrichment(this.selected_data).subscribe((request_id) =>
 
-            this.deepBlueService.getComposedResultIterator(request_id, this.progress_element, 'overlaps_enrichment', this.reloadData, this)
-                .subscribe((result: Object[]) => {
+            this.deepBlueService.getComposedResultIterator(request_id, this.progress_element, 'overlaps_enrichment_fast', this.reloadData, this)
+                .subscribe((result: DeepBlueMiddlewareOverlapEnrichtmentResultItem[]) => {
                     const end = new Date().getTime();
                     // Now calculate and output the difference
                     console.log(result);
+                    this.reloadData(this, result);
                 })
         )
     }
 
-    reloadData(_self: DataSelectionScreen, datum: any) {
-        let biosources_data = datum['biosources'];
+    reloadData(_self: DataSelectionScreen, datum: DeepBlueMiddlewareOverlapEnrichtmentResultItem[]) {
+        if ((!datum) || (datum.length == 0)) {
+            return;
+        }
+        debugger;
+
+        datum.sort((a: DeepBlueMiddlewareOverlapEnrichtmentResultItem, b: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => b.p_value_log - a.p_value_log)
+        let position = 0;
+        let value = datum[0].p_value_log;
+        for (let i = 0; i < datum.length; i++) {
+            if (datum[i].p_value_log != value) {
+                position = i;
+                value = datum[i].p_value_log;
+            }
+            datum[i].log_rank = position + 1;
+        }
+
+        datum.sort((a: DeepBlueMiddlewareOverlapEnrichtmentResultItem, b: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => b.log_odds_ratio - a.log_odds_ratio);
+        position = 0;
+        value = datum[0].log_odds_ratio;
+        for (let i = 0; i < datum.length; i++) {
+            if (datum[i].log_odds_ratio != value) {
+                position = i;
+                value = datum[i].log_odds_ratio;
+            }
+            datum[i]['odd_rank'] = position + 1;
+        }
+
+        datum.sort((a, b) => b['support'] - a['support']);
+        position = 0;
+        value = datum[0]['support'];
+        for (let i = 0; i < datum.length; i++) {
+            if (datum[i]['support'] != value) {
+                position = i;
+                value = datum[i]['support'];
+            }
+            datum[i]['support_rank'] = position + 1;
+        }
+
+        for (let ds of datum) {
+            ds.mean_rank = (ds.log_rank + ds.odd_rank + ds.support_rank) / 3;
+            ds.max_rank = Math.max(ds.log_rank, ds.odd_rank, ds.support_rank);
+        }
+
+        datum.sort((a, b) => a.mean_rank - b.mean_rank);
+
+        let cutoff = Statistics.percentile(datum.map((o: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => o.mean_rank), 0.20);
+        let filtered_data = []
+        for (let d of datum) {
+            if (d["mean_rank"] <= cutoff) {
+                filtered_data.push(d);
+            }
+        }
+
+        console.log(datum.map((o: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => o.mean_rank));
+        console.log(filtered_data.map((o: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => o.mean_rank));
+
+        console.log(cutoff, datum.length, filtered_data.length);
+
+        let biosources: { [key: string]: number[] } = {};
+        let ems : { [key: string]: number[] } = {};
+
+        for (let ds of filtered_data) {
+            let biosource = ds.biosource;
+            let em = ds.epigenetic_mark;
+            let rank = ds.mean_rank;
+
+            if (!(biosource in biosources)) {
+                biosources[biosource] = [];
+            }
+            biosources[biosource].push(rank);
+
+            if (!(em in ems)) {
+                ems[em] = [];
+            }
+            ems[em].push(rank);
+        }
+
+        debugger;
+
+        let biosources_stats: { [key: string]: IStatsResult } = {};
+        let ems_stats : { [key: string]: IStatsResult } = {};
+
+        for (let bs in biosources) {
+            const results = biosources[bs];
+            biosources_stats[bs] = Statistics.calculateStats(biosources[bs]);
+        }
+
+        for (let em in ems) {
+            const results = ems[em];
+            ems_stats[em] = Statistics.calculateStats(ems[em]);
+        }
+
+
+        let biosources_data = Object.keys(biosources_stats).map((biosource) => [biosource, biosources_stats[biosource]]).sort((a: any, b: any) => a[1].mean - b[1].mean);
         _self.plot(biosources_data, _self.biosourcessimilaritybarchart)
 
-        let ems_data = datum['epigenetic_marks'];
+        let ems_data = Object.keys(ems_stats).map((em) => [em, ems_stats[em]]).sort((a: any, b: any) => a[1].mean - b[1].mean);
         _self.plot(ems_data, _self.emssimilaritybarchart)
     }
 
-    plot(datum: any, chart: OverlapsBarChartComponent) {
+    plot(datum: any, chart: SimilarityBarChartComponent) {
         if (!datum) {
             return;
         }
@@ -52,11 +150,9 @@ export class DataSelectionScreen {
 
         const series: Array<Object> = [];
 
-        const stack_values_result: Array<number> = [];
         const stack_values_result_boxplot: Array<Object> = [];
 
         for (let data of datum) {
-            stack_values_result.push(data[1]['mean']);
             stack_values_result_boxplot.push([
                 data[1]['low'],
                 data[1]['q1'],
@@ -72,29 +168,11 @@ export class DataSelectionScreen {
             data: stack_values_result_boxplot,
         });
 
-        series.push({
-            type: 'column',
-            name: "Similar",
-            data: stack_values_result
-        });
+        chart.setNewData(categories, series, null, this.elementClick);
+    }
 
-        let cccategories = ["bone marrow", "myeloid cell"];
-        let ssseries = [
-            {
-                "type": "boxplot",
-                "name": "C000S5H1.ERX149095.H3K27ac.bwa.GRCh38.20150527.bed",
-                "data": [[28615, 29200.5, 29786, 30371.5, 30957], [15299, 22613, 28382, 35165.5, 38309]],
-                "color": "rgba(52,142,0,1)"
-            },
-            {
-                "type": "column",
-                "name": "C000S5H1.ERX149095.H3K27ac.bwa.GRCh38.20150527.bed",
-                "data": [29786, 28221],
-                "color": "rgba(52,142,0,0.3)"
-            }
-        ]
+    elementClick(_self: SimilarityBarChartComponent, event: any) {
 
-        chart.setNewData(categories, series, null);
     }
 
     selectAnnotationForComparison(selectedAnnotation: any) {
