f40037c535363130af3ff4ece514d20f8ffb6cc1
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-01-23 18:33:46 +0100

    Load CpG Islands Annotation from the main screen

--- src/app/view/screen/initial.ts
+++ src/app/view/screen/initial.ts
@@ -2,18 +2,45 @@ import { Component, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { IOperation } from 'app/domain/interfaces';
 import { DeepBlueService } from 'app/service/deepblue';
 import { Router } from '@angular/router';
+import { ProgressElement } from 'app/service/progresselement';
 
 @Component({
   templateUrl: './initial.html'
 })
 export class InitialScreenComponent {
 
-  constructor(private deepBlueService: DeepBlueService, private router: Router) { }
+  constructor(private deepBlueService: DeepBlueService, private progress_element: ProgressElement,
 
-  display: boolean = false;
+    private router: Router) { }
 
-  showDialog() {
-    this.display = true;
+  showDemo: boolean = false;
+  showUpload: boolean = false;
+
+  showDemoDialog() {
+
+    this.deepBlueService.genomeValue$.subscribe(genome => {
+      if (genome === null) {
+        return;
+      }
+      this.deepBlueService.getAnnotations(genome).subscribe(annotations => {
+        for (let annotation of annotations) {
+          if (annotation.name.toLowerCase().startsWith('cpg islands')) {
+            debugger;
+            this.deepBlueService.selectAnnotation(annotation, this.progress_element, 0).subscribe((operation) => {
+              debugger;
+              this.deepBlueService.setDataToDive(operation);
+              this.router.navigate(['/similarfinder']);
+            });
+          }
+        }
+      });
+    });
+
+    this.showDemo = true;
+  }
+
+  showUploadDialog() {
+    this.showUpload = true;
   }
 
   selectQuery(event: IOperation) {
