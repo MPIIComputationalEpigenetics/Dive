ce45d23f86e1a017eaf22593f053a286ad6cd617
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-02-10 04:40:01 +0100

    Use the organization chart for displaying the comparison datasets workflow. It closes #42

--- src/app/view/component/deepblue.ts
+++ src/app/view/component/deepblue.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, ViewChild, OnInit, Input, OnDestroy, Output } from '@angular/core';
 
 import { Subscription } from 'rxjs/Subscription';
 
@@ -20,13 +20,14 @@ import { DeepBlueService } from 'app/service/deepblue';
 import { MultiKeyDataCache } from 'app/service/deepblue';
 import { SelectedData } from 'app/service/selecteddata';
 import { DataStack } from 'app/service/datastack';
+import { EventEmitter } from '@angular/core';
 
 
 @Component({
     selector: 'selected-data-button',
     template: `
     <p-overlayPanel #op [dismissable]="true" [showCloseIcon]="true" appendTo="body">
-        <p-panel [style]="{'width':'500px'}">
+        <p-panel>
             <p-header>
                 <div class="ui-helper-clearfix">
                     <span class="ui-panel-title" style="font-size:16px;display:inline-block;margin-top:2px">{{ dataStack.name() }}</span>
@@ -40,17 +41,11 @@ import { DataStack } from 'app/service/datastack';
                     -->
                 </div>
             </p-header>
+
             <div class="dashboard">
-                <ul class="activity-list" style="background: darkcyan">
+                <ul class="activity-list">
                     <li *ngFor="let data of dataStack._data">
-                        <div class="ui-g">
-                            <div class="ui-g-10">
-                                <p>
-                                    {{ data.what }}
-                                    {{ data.description }}
-                                    {{ data.op.data().name() }} ({{ data.count }})
-                            </div>
-                        </div>
+                        <query-flow [queryId]="data.op.id().id"></query-flow>
                     </li>
                 </ul>
             </div>
@@ -98,17 +93,39 @@ export class SelectedDataButton implements OnInit {
     selector: 'selected-data',
     template: `
                 <p-toolbar>
+                <!-- <p-button label="Include Comparison" styleClass="ui-button-secondary" (onClick)="click($event)"></p-button> -->
                     <div class="ui-toolbar-group-left">
                         <selected-data-button
                             *ngFor="let ds of selectedData._stacks | slice:1"
                             [dataStack]="ds">
                         </selected-data-button>
                     </div>
-                </p-toolbar>
 
+                    <!--
+                    <p-sidebar [(visible)]="includeComparisonBar" position="left" styleClass="ui-sidebar-md">>
+                        <p-scrollPanel [style]="{height: '95%', width: '100%'}">
+                            <div class="card card-w-title">
+                                <h1>Select the data to be used for comparison</h1>
+                                <data-selection-main (queryIdSelected)="selectQuery($event)"></data-selection-main>
+                            </div>
+                        </p-scrollPanel>
+                    </p-sidebar>
+                    -->
+                </p-toolbar>
     `
 })
 export class SelectedDataView {
+
     constructor(public selectedData: SelectedData) { }
+
+    includeComparisonBar = false;
+
+    click($event: any) {
+        this.includeComparisonBar = true;
+    }
+
+    selectQuery($event: any) {
+
+    }
 }
 
