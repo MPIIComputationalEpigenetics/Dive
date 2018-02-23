7709404ec24d4c530262cfabc65901cbd0e7460a
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-02-23 11:19:42 +0900

    feedback!

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -22,23 +22,35 @@ import { timeout } from 'rxjs/operator/timeout';
                     <i class="material-icons">menu</i>
                 </a>
 
-                <div id="rightpanel-menu-button" style="width:250px">
-                    <p-toolbar>
-                        <div class="ui-toolbar-group-left">
-                            <div class="ui-inputgroup">
-                                <span class="ui-inputgroup-addon" style="border-style: none">Genome</span>
-                                <p-dropdown #genomesDropdown [options]="genomeItems" [style]="{'width':'75px'}" (onChange)="selectGenome($event)"  [(ngModel)]="selectedGenome"></p-dropdown>
+                <ul class="topbar-items animated fadeInDown" [ngClass]="{'topbar-items-visible': app.topbarMenuActive}">
+                    <li style="width:175px">
+                        <p-toolbar>
+                            <div class="ui-toolbar-group-left">
+                                <div class="ui-inputgroup">
+                                    <span class="ui-inputgroup-addon" style="border-style: none">Genome</span>
+                                    <p-dropdown #genomesDropdown [options]="genomeItems" [style]="{'width':'75px'}" (onChange)="selectGenome($event)"  [(ngModel)]="selectedGenome"></p-dropdown>
+                                </div>
+                                </div>
+                        </p-toolbar>
+                    </li>
+                    <li style="width:250px">
+                        <p-toolbar>
+                            <div class="ui-toolbar-group-left">
+                                <div class="ui-inputgroup">
+                                    <span class="ui-inputgroup-addon" style="border-style: none">Projects</span>
+                                    <p-multiSelect #multiselect [defaultLabel]="defaultSelectProjects" [options]="projectItems" [(ngModel)]="selectedProjects" (onChange)="selectProjects($event)"></p-multiSelect>
+                                </div>
                             </div>
-                            <div class="ui-inputgroup">
-                                <span class="ui-inputgroup-addon" style="border-style: none">Projects</span>
-                                <p-multiSelect #multiselect [defaultLabel]="defaultSelectProjects" [options]="projectItems" [(ngModel)]="selectedProjects" (onChange)="selectProjects($event)">
-                                </p-multiSelect>
+                        </p-toolbar>
+                    </li>
+                    <li style="width:475px">
+                        <p-toolbar>
+                            <div class="ui-toolbar-group-left">
+                                <label>Dive is in test. Please, give your <a target="_blank" style="color: yellow" href="https://deepblue.userecho.com/">click here for feedback and suggestions</a></label>
                             </div>
-                        </div>
-                    </p-toolbar>
-
-                </div>
-
+                        </p-toolbar>
+                    </li>
+                </ul>
             </div>
         </div>
 
