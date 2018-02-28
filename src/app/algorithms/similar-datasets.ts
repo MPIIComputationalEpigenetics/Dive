eb8df4541a6a63f3e9d8658e108c30a97c858091
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-02-28 12:35:49 +0900

    split the similar datasets logic from similar finder screen,

---
+++ src/app/algorithms/similar-datasets.ts
@@ -0,0 +1,125 @@
+import { DeepBlueService } from "app/service/deepblue";
+import { IOperation } from 'app/domain/interfaces';
+import { ProgressElement } from 'app/service/progresselement';
+import { DeepBlueMiddlewareOverlapEnrichtmentResultItem } from 'app/domain/operations';
+import { Statistics, IStatsResult } from 'app/service/statistics';
+import { BioSource } from 'app/domain/deepblue';
+import { RequestManager } from 'app/service/requests-manager';
+
+export class SimilarDatasets {
+
+  static getOrderFunction(order: string) {
+    if (order == "desc") {
+      return this.desc_func;
+    } else {
+      return this.asc_func;
+    }
+  }
+
+  static desc_func(a: any, b: any, column: string) {
+    return b[column] - a[column];
+  }
+
+  static asc_func(a: any, b: any, column: string) {
+    return a[column] - b[column];
+  }
+
+  static sortDatasets(cutoffValue: number, order: string, datum: DeepBlueMiddlewareOverlapEnrichtmentResultItem[]) {
+
+    let orderFunction = this.getOrderFunction(order);
+
+    if ((!datum) || (datum.length == 0)) {
+      return {
+        "all": [],
+        "biosources": [],
+        "epigenetic_marks": []
+      }
+    }
+
+    datum.sort((a, b) => orderFunction(a, b, 'p_value_log'));
+    let position = 0;
+    let value = datum[0].p_value_log;
+    for (let i = 0; i < datum.length; i++) {
+      if (datum[i].p_value_log != value) {
+        position = i;
+        value = datum[i].p_value_log;
+      }
+      datum[i].log_rank = position + 1;
+    }
+
+    datum.sort((a, b) => orderFunction(a, b, 'odds_ratio'));
+    position = 0;
+    value = datum[0].odds_ratio;
+    for (let i = 0; i < datum.length; i++) {
+      if (datum[i].odds_ratio != value) {
+        position = i;
+        value = datum[i].odds_ratio;
+      }
+      datum[i].odd_rank = position + 1;
+    }
+
+    datum.sort((a, b) => orderFunction(a, b, 'support'));
+    position = 0;
+    value = datum[0].support;
+    for (let i = 0; i < datum.length; i++) {
+      if (datum[i].support != value) {
+        position = i;
+        value = datum[i].support;
+      }
+      datum[i].support_rank = position + 1;
+    }
+
+    for (let ds of datum) {
+      ds.mean_rank = (ds.log_rank + ds.odd_rank + ds.support_rank) / 3;
+      ds.max_rank = Math.max(ds.log_rank, ds.odd_rank, ds.support_rank);
+    }
+
+    datum.sort((a, b) => a.mean_rank - b.mean_rank);
+
+    let cutoff = Statistics.percentile(datum.map((o: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => o.mean_rank), (cutoffValue / 100));
+    let filtered_data = []
+    for (let d of datum) {
+      if (d["mean_rank"] <= cutoff) {
+        filtered_data.push(d);
+      }
+    }
+
+    let biosources: { [key: string]: number[] } = {};
+    let ems: { [key: string]: number[] } = {};
+
+    for (let ds of filtered_data) {
+      let biosource = ds.biosource;
+      let em = ds.epigenetic_mark;
+      let rank = ds.mean_rank;
+
+      if (!(biosource in biosources)) {
+        biosources[biosource] = [];
+      }
+      biosources[biosource].push(rank);
+
+      if (!(em in ems)) {
+        ems[em] = [];
+      }
+      ems[em].push(rank);
+    }
+
+    let biosources_stats: { [key: string]: IStatsResult } = {};
+    let ems_stats: { [key: string]: IStatsResult } = {};
+
+    for (let bs in biosources) {
+      const results = biosources[bs];
+      biosources_stats[bs] = Statistics.calculateStats(biosources[bs]);
+    }
+
+    for (let em in ems) {
+      const results = ems[em];
+      ems_stats[em] = Statistics.calculateStats(ems[em]);
+    }
+
+    return {
+      "all": datum,
+      "biosources": Object.keys(biosources_stats).map((biosource) => [biosource, biosources_stats[biosource]]).sort((a: any, b: any) => a[1].mean - b[1].mean),
+      "epigenetic_marks": Object.keys(ems_stats).map((em) => [em, ems_stats[em]]).sort((a: any, b: any) => a[1].mean - b[1].mean)
+    }
+  }
+}
--- src/app/view/screen/similar-finder.ts
+++ src/app/view/screen/similar-finder.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 
 import { ConfirmDialogModule, ConfirmationService } from 'primeng/primeng';
 import { MenuItem } from 'primeng/primeng';
@@ -12,12 +12,13 @@ import { Statistics, IStatsResult } from 'app/service/statistics';
 import { BioSource } from 'app/domain/deepblue';
 import { RequestManager } from 'app/service/requests-manager';
 import { Subscription } from 'rxjs';
-import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { SimilarDatasets } from '../../algorithms/similar-datasets';
 
 @Component({
     templateUrl: './similar-finder.html'
 })
 export class SimilarFinder implements OnDestroy {
+
 
     @ViewChild('biosourcessimilaritybarchart') biosourcessimilaritybarchart: SimilarityBarChartComponent;
     @ViewChild('emssimilaritybarchart') emssimilaritybarchart: SimilarityBarChartComponent;
@@ -38,11 +39,11 @@ export class SimilarFinder implements OnDestroy {
     cutoffValue = 20;
 
     orderOptions = [
-        {label: 'Most similar', value: this.desc_func},
-        {label: 'Most dissimilar', value: this.asc_func}
+        {label: 'Most similar', value: "desc"},
+        {label: 'Most dissimilar', value: "asc"}
     ];
 
-    orderFunction = this.desc_func;
+    orderDirection = "desc";
 
     prevDatum: DeepBlueMiddlewareOverlapEnrichtmentResultItem[] = null;
 
@@ -63,20 +64,13 @@ export class SimilarFinder implements OnDestroy {
 
     processSimilar(data: IOperation) {
         this.deepBlueService.composedCalculateFastsEnrichment(data).subscribe((request) => {
+            this.requestManager.cancelAllRequest();
             this.requestManager.enqueueRequest(request)
             this.deepBlueService.getComposedResultIterator(request, this.progress_element, 'overlaps_enrichment_fast', this.reloadData, this)
                 .subscribe((result: DeepBlueMiddlewareOverlapEnrichtmentResultItem[]) => {
                     this.reloadData(this, result);
                 })
         })
-    }
-
-    desc_func(a: any, b: any, column: string) {
-        return b[column] - a[column];
-    }
-
-    asc_func(a: any, b: any, column: string) {
-        return a[column] - b[column];
     }
 
     reloadData(_self: SimilarFinder, datum: DeepBlueMiddlewareOverlapEnrichtmentResultItem[]) {
@@ -94,91 +88,10 @@ export class SimilarFinder implements OnDestroy {
             return;
         }
 
-        datum.sort((a, b) => _self.orderFunction(a, b, 'p_value_log'));
-        let position = 0;
-        let value = datum[0].p_value_log;
-        for (let i = 0; i < datum.length; i++) {
-            if (datum[i].p_value_log != value) {
-                position = i;
-                value = datum[i].p_value_log;
-            }
-            datum[i].log_rank = position + 1;
-        }
+        let sortedData = SimilarDatasets.sortDatasets(_self.cutoffValue, _self.orderDirection, datum);
 
-        datum.sort((a, b) => _self.orderFunction(a, b, 'odds_ratio'));
-        position = 0;
-        value = datum[0].odds_ratio;
-        for (let i = 0; i < datum.length; i++) {
-            if (datum[i].odds_ratio != value) {
-                position = i;
-                value = datum[i].odds_ratio;
-            }
-            datum[i].odd_rank = position + 1;
-        }
-
-        datum.sort((a, b) => _self.orderFunction(a, b, 'support'));
-        position = 0;
-        value = datum[0].support;
-        for (let i = 0; i < datum.length; i++) {
-            if (datum[i].support != value) {
-                position = i;
-                value = datum[i].support;
-            }
-            datum[i].support_rank = position + 1;
-        }
-
-        for (let ds of datum) {
-            ds.mean_rank = (ds.log_rank + ds.odd_rank + ds.support_rank) / 3;
-            ds.max_rank = Math.max(ds.log_rank, ds.odd_rank, ds.support_rank);
-        }
-
-        datum.sort((a, b) => a.mean_rank - b.mean_rank);
-
-        let cutoff = Statistics.percentile(datum.map((o: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => o.mean_rank), (_self.cutoffValue / 100));
-        let filtered_data = []
-        for (let d of datum) {
-            if (d["mean_rank"] <= cutoff) {
-                filtered_data.push(d);
-            }
-        }
-
-        let biosources: { [key: string]: number[] } = {};
-        let ems: { [key: string]: number[] } = {};
-
-        for (let ds of filtered_data) {
-            let biosource = ds.biosource;
-            let em = ds.epigenetic_mark;
-            let rank = ds.mean_rank;
-
-            if (!(biosource in biosources)) {
-                biosources[biosource] = [];
-            }
-            biosources[biosource].push(rank);
-
-            if (!(em in ems)) {
-                ems[em] = [];
-            }
-            ems[em].push(rank);
-        }
-
-        let biosources_stats: { [key: string]: IStatsResult } = {};
-        let ems_stats: { [key: string]: IStatsResult } = {};
-
-        for (let bs in biosources) {
-            const results = biosources[bs];
-            biosources_stats[bs] = Statistics.calculateStats(biosources[bs]);
-        }
-
-        for (let em in ems) {
-            const results = ems[em];
-            ems_stats[em] = Statistics.calculateStats(ems[em]);
-        }
-
-        let biosources_data = Object.keys(biosources_stats).map((biosource) => [biosource, biosources_stats[biosource]]).sort((a: any, b: any) => a[1].mean - b[1].mean);
-        _self.plot("Similar BioSources", biosources_data, _self.biosourcessimilaritybarchart, _self.biosourceElementClick)
-
-        let ems_data = Object.keys(ems_stats).map((em) => [em, ems_stats[em]]).sort((a: any, b: any) => a[1].mean - b[1].mean);
-        _self.plot("Similar Epigenetic Marks", ems_data, _self.emssimilaritybarchart, _self.epigeneticMarkElementClick)
+        _self.plot("Similar BioSources", sortedData['biosources'], _self.biosourcessimilaritybarchart, _self.biosourceElementClick)
+        _self.plot("Similar Epigenetic Marks", sortedData['epigenetic_marks'], _self.emssimilaritybarchart, _self.epigeneticMarkElementClick)
     }
 
     plot(title: string, datum: any, chart: SimilarityBarChartComponent, clickCallback: any) {
