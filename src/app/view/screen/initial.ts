74a4d91f92a9616f56351f5282bd373209ffc2c1
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-03-03 01:17:23 +0900

    initial screen clean up

--- src/app/view/screen/initial.ts
+++ src/app/view/screen/initial.ts
@@ -1,51 +1,9 @@
-import { Component, OnDestroy, Output, EventEmitter } from '@angular/core';
-import { IOperation } from 'app/domain/interfaces';
-import { DeepBlueService } from 'app/service/deepblue';
-import { Router } from '@angular/router';
-import { ProgressElement } from 'app/service/progresselement';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './initial.html'
 })
 export class InitialScreenComponent {
 
-  constructor(private deepBlueService: DeepBlueService, private progress_element: ProgressElement,
-    private router: Router) { }
-
-  showDemo: boolean = false;
-  showUpload: boolean = false;
-
-  showDemoDialog() {
-
-    this.deepBlueService.genomeValue$.subscribe(genome => {
-      if (genome === null) {
-        return;
-      }
-      this.deepBlueService.getAnnotations(genome).subscribe(annotations => {
-        for (let annotation of annotations) {
-          if (annotation.name.toLowerCase().startsWith('cpg islands')) {
-            this.deepBlueService.selectAnnotation(annotation, this.progress_element, 0).subscribe((operation) => {
-              this.deepBlueService.setDataToDive(operation);
-              this.router.navigate(['/similarfinder']);
-            });
-          }
-        }
-      });
-    });
-
-    this.showDemo = true;
-  }
-
-  showExistingDataset() {
-    this.router.navigate(['/dataselection']);
-  }
-
-  showUploadDialog() {
-    this.showUpload = true;
-  }
-
-  selectQuery(event: IOperation) {
-    this.deepBlueService.setDataToDive(event);
-    this.router.navigate(['/similarfinder']);
-  }
+  constructor() { }
 }
