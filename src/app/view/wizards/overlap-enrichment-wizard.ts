9d4e9cb44212b0c5164d4b553d48a7c8837feb12
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-04-02 00:16:11 +0200

    Working version of overlap enrichment wizard

--- src/app/view/wizards/overlap-enrichment-wizard.ts
+++ src/app/view/wizards/overlap-enrichment-wizard.ts
@@ -2,16 +2,17 @@ import { Component, OnDestroy, AfterViewInit, ViewChild, Inject, forwardRef, Inp
 import { DeepBlueService } from "app/service/deepblue";
 import { SelectItem, Dropdown, MultiSelect } from "primeng/primeng";
 import { Project, BioSource } from "app/domain/deepblue";
-import { IOperation } from "app/domain/interfaces";
+import { IOperation, IRow } from "app/domain/interfaces";
 import { WizardComponent } from "ng2-archwizard";
 import { AppComponent } from "app/app.component";
 import { Router } from "@angular/router";
 import { ProgressElement } from "app/service/progresselement";
 import { SimilarDatasets } from "app/algorithms/similar-datasets";
-import { DeepBlueMiddlewareOverlapEnrichtmentResultItem } from "app/domain/operations";
+import { DeepBlueMiddlewareOverlapEnrichtmentResultItem, DeepBlueMiddlewareRequest, DeepBlueMiddlewareOverlapEnrichtmentResult } from "app/domain/operations";
 import { RequestManager } from "../../service/requests-manager";
 import { IStatsResult } from "app/service/statistics";
 import { SelectedData } from "app/service/selecteddata";
+import { Utils } from "app/service/utils";
 
 @Component({
   selector: 'overlap-enrichment-wizard',
@@ -20,13 +21,20 @@ import { SelectedData } from "app/service/selecteddata";
 export class OverlapEnrichmentWizard {
 
   finished: boolean = true;
-  selected_data: IOperation;
+  visibleWizard: boolean = true;
+
   background: IOperation;
+  selected_datasets = new Array<Object>();
+  enrichment_data: Object[][] = new Array<Object[]>();
+
+  enrichment_data_from_server = new Array<IRow[]>();
+
+  columns = DeepBlueMiddlewareOverlapEnrichtmentResultItem.asColumns();
 
   @ViewChild('wizard') wizard: WizardComponent;
 
   constructor(@Inject(forwardRef(() => AppComponent)) public app: AppComponent,
-    public progress_element: ProgressElement, private selectedData: SelectedData,
+    public progress_element: ProgressElement, public selectedData: SelectedData,
     private requestManager: RequestManager, public deepBlueService: DeepBlueService) {
   }
 
@@ -44,13 +52,16 @@ export class OverlapEnrichmentWizard {
     this.background = event;
   }
 
-  selectQuery(event: IOperation) {
-    this.selected_data = event;
+  selectDatasets(event: Object[]) {
+    console.log(event);
+    this.selected_datasets = event;
   }
 
   finishWizard($event: any) {
     setTimeout(() => this.app.onMenuButtonClick());
     this.finished = true;
+    this.startEnrichment();
+    this.visibleWizard = false;
   }
 
   getDatasetsLabel() {
@@ -58,8 +69,56 @@ export class OverlapEnrichmentWizard {
   }
 
   startEnrichment() {
-    console.log("aaaaa");
+    const current: IOperation[] = this.selectedData.getStacksTopOperation();
+
+    this.deepBlueService.composedCalculateOverlapsEnrichment(current, this.background.id(), this.selected_datasets)
+      .subscribe((request: DeepBlueMiddlewareRequest) => {
+        this.requestManager.enqueueRequest(request);
+        this.deepBlueService.getComposedResultIterator(request, 'overlaps_enrichment')
+          .subscribe((result: DeepBlueMiddlewareOverlapEnrichtmentResult[]) => {
+            this.prepare_data(result);
+          });
+      });
   }
+
+
+  prepare_data(datum: DeepBlueMiddlewareOverlapEnrichtmentResult[]) {
+
+    this.enrichment_data_from_server = [];
+
+    for (let pos = 0; pos < datum.length; pos++) {
+      const data = datum[pos];
+      const rows = data.getResults().map((x: DeepBlueMiddlewareOverlapEnrichtmentResultItem) => {
+        const row: IRow = {};
+        for (let idx = 0; idx < this.columns.length; idx++) {
+          const column_name = this.columns[idx].name;
+          const v = x.data[column_name];
+          row[column_name.toLowerCase().replace(/_/g, '')] = Utils.convert(v, this.columns[idx]['column_type'])
+        }
+        return row;
+      });
+
+      rows.sort((a: IRow, b: IRow) => a['meanrank'] - b['meanrank']);
+
+      this.enrichment_data_from_server.push(rows);
+    }
+
+    this.filter_enrichment_data(null);
+  }
+
+
+  filter_enrichment_data($event: any) {
+    const newResults = [];
+    for (let idx = 0; idx < this.enrichment_data_from_server.length; idx++) {
+      //const x = this.filter_enrichment_datei(this.enrichment_data_from_server[idx]);
+      const x = this.enrichment_data_from_server[idx];
+      newResults.push(x);
+    }
+
+    this.enrichment_data = newResults;
+    //this.plotBar();
+  }
+
 
   wizardClassLength(): string {
     if (this.finished) {
